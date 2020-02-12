bc13a85ca17bf6b79dd9deaf6e4d4fc8d7bad1fd
Nicholas Dongoske <nickdongoske@gmail.com>
2020-02-12 04:35:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -59,14 +59,12 @@ end
 def reduce_to_all_true(source_array) 
  i = 0
  while i < source_array.length do
-   if source_array[i] == true
-     return true
-  if source_array[i] == false
-    return false
-   end
+   return false if !source_array[i]
    
    i += 1
   end
+  return true
+end
   
 
 def reduce_to_any_true(source_array)
