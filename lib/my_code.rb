2f52d3ffc4ef2f5c164b519aa476a95a5e9df312
Nicholas Dongoske <nickdongoske@gmail.com>
2020-02-12 04:17:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -49,7 +49,7 @@ def reduce_to_total(source_array, starting_point = 0)
   i = 0
   new_array = []
   while i < source_array.length do
-    source_array.map 
+    source_array[i].map 
     
     i += 1
   end
